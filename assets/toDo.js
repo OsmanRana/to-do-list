642b39dd14407fbf3f24ad00bf1f689d7e23899c
OsmanRana <osmancmc@yahoo.com>
2022-08-20 23:16:21 +0600

    add function added

--- assets/toDo.js
+++ assets/toDo.js
@@ -6,12 +6,18 @@ main.innerHTML = `<h1>To Do List</h1>
 <button id=btn-add>Add</button>
 `
 const taskToAdd = document.getElementById('task')
-
-const addButton = document.getElementById('btn-add').addEventListener('click', function(){
-    let taskToAddValue = taskToAdd.value;
+function addTask (){
+    const taskToAddValue = taskToAdd.value;
     const p = document.createElement('p');
     p.innerText = taskToAddValue;
     main.appendChild(p);
     taskToAdd.value = ""
-    console.log('clicked', p)
+}
+document.addEventListener('keypress', function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+})
+const addButton = document.getElementById('btn-add').addEventListener('click', function(event){
+    addTask();
 })
