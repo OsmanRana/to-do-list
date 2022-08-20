8ef15fa30e0ecb82026ed3b0113560eb3be9c9cd
OsmanRana <osmancmc@yahoo.com>
2022-08-20 22:07:31 +0600

    js added'

--- assets/toDo.js
+++ assets/toDo.js
@@ -1,1 +1,13 @@
-console.log('To Do')
+const main = document.getElementById('main');
+main.innerHTML = `<h1>To Do List</h1>
+<br>
+<input id="task" type="text">
+<br>
+<button id=btn-add>Add</button>
+`
+const taskToAdd = document.getElementById('task')
+const taskToAddValue = taskToAdd.value;
+const addButton = document.getElementById('btn-add').addEventListener('click', function(){
+    console.log('clicked')
+})
+console.log(addButton)
