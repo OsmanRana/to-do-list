03d59175aaaff69a8790eb830160c08098fc48e0
OsmanRana <osmancmc@yahoo.com>
2022-08-22 15:38:09 +0600

    Hide Show added

--- assets/toDo.js
+++ assets/toDo.js
@@ -1,34 +1,48 @@
 const main = document.getElementById('main');
 main.innerHTML = `<h1>To Do List</h1>
-<br>
 <input id="task" type="text">
-<br>
 <button id=btn-add>Add</button>
+<button id=btn-show-hide>Hide</button>
 `
 const taskToAdd = document.getElementById('task');
-function addTask (){
-    
-    const taskToAddValue = taskToAdd.value;
-    const div = document.createElement('div');
-    div.setAttribute("id", "task-container");
-    main.appendChild(div);
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    li.innerText = taskToAddValue;
-    ul.appendChild(li)    
-    const button = document.createElement('button');
-    button.setAttribute("id", "btn-remove");
-    button.innerText = "Remove";
-    div.appendChild(ul);
-    div.appendChild(button);
-    taskToAdd.value = ""
 
-    document.getElementById('btn-remove').addEventListener('click', function(){
-        const taskContainer = document.getElementById('task-container');
-        document.removeChild(taskContainer)
-        console.log('clicked', taskContainer);
-    })
+const div = document.createElement('div');
+div.setAttribute("id", "task-container");
+main.appendChild(div);
+
+function addTask (){
+    const taskToAddValue = taskToAdd.value;
+   
+    const ul = document.createElement('ul');
+    ul.setAttribute("id", "task-ul")
+    const li = document.createElement('li');
+    li.classList = "movingLi"
+    li.innerText = taskToAddValue;
+    const button = document.createElement('button')
+    button.classList =  "btn-remove";
+    button.innerText = "Remove";
+    ul.appendChild(li)    
+    ul.appendChild(button);
+    div.appendChild(ul);
+    taskToAdd.value = ""
+  
 }
+let isShow = true;
+
+const showHideButton = document.getElementById("btn-show-hide").addEventListener("click", function(){
+    const showHideDiv = document.getElementById("task-container")
+    let buttonText = document.getElementById("btn-show-hide");
+    if(isShow){
+        showHideDiv.style.display = "none"
+        isShow = false;
+        buttonText.innerText = "Show"
+    }else if(isShow !== true){
+        showHideDiv.style.display = "block"
+        isShow = true;
+        buttonText.innerText = "Hide"
+    }
+
+})
 document.addEventListener('keypress', function(event){
     if(taskToAdd.value === ""){
         return;
@@ -38,7 +52,7 @@ document.addEventListener('keypress', function(event){
         addTask();
     }
 })
-const addButton = document.getElementById('btn-add').addEventListener('click', function(event){
+document.getElementById('btn-add').addEventListener('click', function(){
     if(taskToAdd.value === ""){
         return;
     }
