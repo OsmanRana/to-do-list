c99415111498683c473b24902ca283f2cc0f4f15
OsmanRana <osmancmc@yahoo.com>
2022-08-22 21:18:47 +0600

    section added

--- assets/toDo.js
+++ assets/toDo.js
@@ -13,6 +13,20 @@ const div = document.createElement('div');
 div.setAttribute("id", "task-container");
 main.appendChild(div);
 
+const newTaskDiv = document.createElement('div');
+newTaskDiv.innerHTML = `<h3>New Task</h3>`
+const inProgressDiv = document.createElement('div');
+inProgressDiv.innerHTML = `<h3>Task In Progress</h3>`
+const canceledDiv = document.createElement('div');
+canceledDiv.innerHTML = `<h3>Canceled Task</h3>`
+const completedDiv = document.createElement('div');
+completedDiv.innerHTML = `<h3>Completed Task</h3>`
+
+div.appendChild(newTaskDiv);
+div.appendChild(inProgressDiv);
+div.appendChild(canceledDiv);
+div.appendChild(completedDiv);
+
 function addTask (){
     const taskToAddValue = taskToAdd.value;
     const newTaskUl = document.createElement('ul');
@@ -31,7 +45,7 @@ function addTask (){
     newTaskLi.appendChild(removeButton);
     newTaskLi.appendChild(startButton);
     newTaskUl.appendChild(newTaskLi);    
-    div.appendChild(newTaskUl);
+    newTaskDiv.appendChild(newTaskUl);
     taskToAdd.value = ""
 
     startButton.addEventListener('click', function(){
@@ -40,8 +54,30 @@ function addTask (){
         inProgressUl.setAttribute("id", "in-progress-ul");
         const inProgressLi = document.createElement('li');
         inProgressLi.innerText = newTaskLi.childNodes[0].data;
-        inProgressUl.appendChild(inProgressLi)
-        div.appendChild(inProgressUl)
+        const completedButton = document.createElement('button');
+        completedButton.innerText = "Finish";
+        inProgressLi.appendChild(completedButton);
+        inProgressUl.appendChild(inProgressLi);
+        inProgressDiv.appendChild(inProgressUl);
+
+        completedButton.addEventListener("click", function(){
+            inProgressUl.removeChild(inProgressLi);
+            const completedUl = document.createElement("ul");
+            const completedLi = document.createElement("li");
+            completedLi.innterText = inProgressLi.innterText;
+            console.dir(inProgressLi)
+            completedUl.appendChild(completedLi);
+            completedDiv.appendChild(completedUl);
+        })
+    }) 
+    removeButton.addEventListener('click', function(){
+        newTaskUl.removeChild(newTaskLi);
+        const canceledUl = document.createElement('ul');
+        canceledUl.setAttribute("id", "canceled-ul");
+        const canceledLi = document.createElement('li');
+        canceledLi.innerText = newTaskLi.childNodes[0].data;
+        canceledUl.appendChild(canceledLi)
+        canceledDiv.appendChild(canceledUl)
     }) 
 }
 let isShow = true;
